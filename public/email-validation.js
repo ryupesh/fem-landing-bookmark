5cbfe95d0e69274e511a0cad46da8fff4d5e6139
Rupesh Pawar <115rupesh@gmail.com>
2022-09-05 16:24:13 +0530

    refactored email-validation js and minor visual bug fixes

--- public/email-validation.js
+++ public/email-validation.js
@@ -4,58 +4,62 @@ const errMsg = document.querySelector("[data-error]");
 const btnContact = document.querySelector("[data-btn-contact]");
 const pattern =
   /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  
-  
-//   on err => 
-//   inputGroup container = bg-red-500 rounded-lg
-//   data-email = border-2 border-red-500
-//   data-error = remove(hidden)
-const showError = function (msg) {
-  emailInput.parentElement.classList.add("bg-red-500");
-  emailInput.parentElement.classList.add("rounded-lg");
 
-  emailInput.classList.add("border-2");
-  emailInput.classList.add("border-red-500");
-
+// show error message
+const showErrorMsg = function (msg) {
+  emailInput.classList.add("border-2", "border-red-500");
+  emailInput.parentElement.classList.add("bg-red-500", "rounded-lg");
   emailInput.nextElementSibling.classList.remove("hidden");
 
   errMsg.innerHTML = msg;
 };
 
-const noError = function () {
-  emailInput.parentElement.classList.remove("bg-red-500");
-  emailInput.parentElement.classList.add("rounded-lg");
-
-  emailInput.classList.remove("border-2");
-  emailInput.classList.remove("border-red-500");
-
+// hide error message
+const clearErrorMsg = function () {
+  emailInput.classList.remove("border-2", "border-red-500");
+  emailInput.parentElement.classList.remove("bg-red-500", "rounded-lg");
   emailInput.nextElementSibling.classList.add("hidden");
 };
 
+// show success message
+const showSuccessMsg = function () {
+  const successToast = document.createElement("div");
+  successToast.className =
+    "p-2 my-8 bg-green-300 text-green-800 rounded-lg border border-green-800";
+  successToast.setAttribute("data-success", "");
+  let html = `
+  <span>${emailInput.value} has been added. Thank you!</span>
+  `;
+  successToast.innerHTML = html;
+  emailForm.parentElement.append(successToast);
+};
+
+// validation logic for  email id
 const validateEmail = function () {
   // check empty
   if (emailInput.value.trim().length === 0) {
-    showError("Email can not be empty!");
+    showErrorMsg("Email can not be empty!");
   } else if (pattern.test(emailInput.value)) {
-    noError();
+    clearErrorMsg();
     return true;
   } else {
-    showError("Whoops! your email id is invalid!");
+    showErrorMsg("Whoops! your email id is invalid!");
   }
 };
 
+// on input from user validate email id and show/hide error msgs
 emailInput.addEventListener("keyup", () => {
   validateEmail();
 });
 
+// on submit
 emailForm.addEventListener("submit", (ev) => {
   ev.preventDefault();
   if (validateEmail()) {
+    showSuccessMsg();
     emailForm.reset();
-    btnContact.classList.add("hidden");
-    emailInput.classList.add("bg-green-500");
-    emailInput.classList.add("text-white");
-    emailInput.value="Your email id is submitted!"
-    emailInput.setAttribute("disabled","");
+    setTimeout(() => {
+      document.querySelector("[data-success]").remove();
+    }, 4000);
   }
 });
